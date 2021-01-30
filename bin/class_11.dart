eea20ab4b0adbf39ada3766c992a4b038760e51b
Faisal <nirzon192@gmail.com>
2021-01-30 18:35:39 +0600

    5th

--- bin/class_11.dart
+++ bin/class_11.dart
@@ -7,9 +7,22 @@ import 'dart:io';
 void main(List<String> arguments) {
   List<String>  flutterBatch = ["DR","Sifat","Adul","zanth","Abu"];
 
-  for (String prop in flutterBatch) {
+  /*for (String prop in flutterBatch) {
     print(prop);
+  }*/
+  int i= 0;
+  while(i<flutterBatch.length){
+    //print(flutterBatch[i]);
+    print(i);
+    print("${++i}") ;
+    print(i);
   }
+   i= 0;
+  /*do {
+    print("Do while ${flutterBatch[i]}");
+    ++i;
+  }while(i<flutterBatch.length);*/
+
   /*for( int i= 0;i<flutterBatch.length;i++ ){
     print(flutterBatch[i]);
   }*/
